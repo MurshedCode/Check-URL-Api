9c59f7b015d37a0e191d432a66cfb042b402bb64
Murshed <imagi@DESKTOP-SB6EI0J>
2019-06-11 12:51:44 +0100

    Test fails before method implementations following approach Red Green and Refactor

---
+++ Check URL Api v1.0/testsrc/com/murshedcode/UrlContentCheckAppTest.java
@@ -0,0 +1,31 @@
+package com.murshedcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.nio.charset.MalformedInputException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UrlContentCheckAppTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test(expected  = MalformedInputException.class)
+	void validateUrlExceptionTest() throws MalformedInputException
+	{
+		// check youtube and implement
+		
+	}
+
+
+
+}
--- Check URL Api v1.0/testsrc/com/murshedcode/UrlHttpRequestTest.java
+++ Check URL Api v1.0/testsrc/com/murshedcode/UrlHttpRequestTest.java
@@ -1,6 +1,11 @@
 package com.murshedcode;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,9 +20,51 @@ class UrlHttpRequestTest {
 		
 	}
 
+	
+	
+	// checking return array from Get request
 	@Test
-	void testgetStatus() {
-		fail("Not yet implemented");
+	void testValidUrlStatus() {
+		
+		String url = "https://paypal.com";
+		long[] stautsArray = UrlHttpRequestUnderTest.getStatus(url);	
+		assertEquals(HttpURLConnection.HTTP_OK, stautsArray[0]);
+		
 	}
 
+	
+	@Test
+	void testInValidUrlStatus() {
+		
+		String url = "http://not.exists.bbc.co.uk";
+		long[] stautsArray = UrlHttpRequestUnderTest.getStatus(url);	
+		assertEquals(HttpURLConnection.HTTP_UNSUPPORTED_TYPE, stautsArray[0]);
+		
+	}
+	
+	
+	
+	
+	@Test
+	void testURL() throws Exception {
+	    String strUrl = "https://google.com";
+
+	    try {
+	        URL url = new URL(strUrl);
+	        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+	        urlConn.connect();
+
+	        assertEquals(HttpURLConnection.HTTP_OK, urlConn.getResponseCode());
+	    } catch (IOException e) {
+	        System.err.println("Error creating HTTP connection");
+	        e.printStackTrace();
+	        throw e;
+	    }
+	}
+	
+	
+	
+	
+	
+	
 }
