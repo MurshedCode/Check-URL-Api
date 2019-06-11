fd857a437b422aadaa48a62e0aa848f16485ee2b
Murshed <imagi@DESKTOP-SB6EI0J>
2019-06-11 14:21:31 +0100

    Dependencies and jar files added/method implemented for JsonObject

--- Check URL Api v1.0/src/com/murshedcode/UrlHttpRequest.java
+++ Check URL Api v1.0/src/com/murshedcode/UrlHttpRequest.java
@@ -1,4 +1,13 @@
 package com.murshedcode;
+
+import java.io.BufferedInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+
+import org.json.JSONObject;
+
 /**
  *  This is helper class with utility methods implementation to fetch url header response data 
  * - Create an array elements with response 
@@ -6,7 +15,6 @@ package com.murshedcode;
  * 
  * @author Murshed
  */
-
 
 
 
@@ -19,10 +27,48 @@ public class UrlHttpRequest {
 	
 	public static long[] getStatus(String url) {
 		
-		return null;
-		
-		
+		long[] stautsArray = new long[4];
+
+		try {
+
+			URL siteURL = new URL(url);
+			HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(10000); // timeout after 10 seconds
+			connection.connect();
+
+
+			stautsArray[0] = connection.getResponseCode();
+			BufferedInputStream bif = new BufferedInputStream(connection.getInputStream());
+			stautsArray[1] = bif.available();
+			stautsArray[2] =  connection.getDate();
+
+		} catch (MalformedURLException e) {
+		}
+		catch (Exception e) {
+			System.out.println(url + "\t\t Erorr: invalid url... " + e.getMessage()); 
+		}
+		return stautsArray;
 	}
 	
+	
+	static JSONObject getJsonObject(String url) throws Exception{
+		JSONObject responseObject = null;
+		long[] urLStatus = getStatus(url);
+		// Create Json Object and print in console
 
+		responseObject = new JSONObject();
+		responseObject.put("URL", url);
+		responseObject.put("Status_code", urLStatus[0]);
+		responseObject.put("Date", new Date (urLStatus[1]));
+		responseObject.put("Content_length", urLStatus[2]);
+
+
+		// Can check response type and print response as Json Object
+
+		System.out.println(responseObject);
+
+		return responseObject;  // to check and implement unit test on return object
+
+	}
 }
--- Check URL Api v1.0/testsrc/com/murshedcode/UrlHttpRequestTest.java
+++ Check URL Api v1.0/testsrc/com/murshedcode/UrlHttpRequestTest.java
@@ -1,7 +1,6 @@
 package com.murshedcode;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -21,30 +20,6 @@ class UrlHttpRequestTest {
 	}
 
 	
-	
-	// checking return array from Get request
-	@Test
-	void testValidUrlStatus() {
-		
-		String url = "https://paypal.com";
-		long[] stautsArray = UrlHttpRequestUnderTest.getStatus(url);	
-		assertEquals(HttpURLConnection.HTTP_OK, stautsArray[0]);
-		
-	}
-
-	
-	@Test
-	void testInValidUrlStatus() {
-		
-		String url = "http://not.exists.bbc.co.uk";
-		long[] stautsArray = UrlHttpRequestUnderTest.getStatus(url);	
-		assertEquals(HttpURLConnection.HTTP_UNSUPPORTED_TYPE, stautsArray[0]);
-		
-	}
-	
-	
-	
-	
 	@Test
 	void testURL() throws Exception {
 	    String strUrl = "https://google.com";
@@ -56,11 +31,51 @@ class UrlHttpRequestTest {
 
 	        assertEquals(HttpURLConnection.HTTP_OK, urlConn.getResponseCode());
 	    } catch (IOException e) {
-	        System.err.println("Error creating HTTP connection");
 	        e.printStackTrace();
 	        throw e;
 	    }
+	
 	}
+	
+	
+	// checking return array from Get request
+	
+	@Test
+	void testGetContentOk() throws Exception {
+		String url = "https://paypal.com";
+		long[] stautsArray = UrlHttpRequestUnderTest.getStatus(url);	
+		assertEquals("Valid url should return 200 for array elemnt", HttpURLConnection.HTTP_OK, stautsArray[0]);
+		
+	}
+
+	
+	@Test
+	void testDoesNotGetContentOk() throws Exception {
+		String url = "http:// not.exists.bbc.co.uk";
+		long[] stautsArray = UrlHttpRequestUnderTest.getStatus(url);	
+		assertEquals("Invalid URL should return 0 for array element", 0, stautsArray[0]);
+		
+	}
+	
+	
+	// @Test Catch Exception invalid URL
+	
+	
+	// @ Test connection timeout 
+	
+	@Test
+	void testJsonObject()throws Exception {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
